4e99f7d850405c306cd8c4f7f3c0e23a71b41b9f
Shawna Spoor <shawna@spoor.org>
2013-11-03 11:44:30 -0500

    profile update functionality added without ability to stop empty fields

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -60,6 +60,51 @@
 
 		}
 
+		public function p_profile_update() {
+			
+			echo"<pre>";
+			print_r($this->user);
+			echo"<pre>";
+
+			$data    = Array(
+				
+				"user_id" => $this->user->user_id, 
+				"first_name" => $_POST['first_name'], 
+				"last_name" => $_POST['last_name'], 
+				"email" => $_POST['email']
+				);
+
+
+	        $user_id = DB::instance(DB_NAME)->update_or_insert_row("users", $data);
+			
+			
+
+			/*$q = "SELECT
+				users.first_name,
+				users.last_name,
+				users.email,
+				users.user_id
+				FROM users
+				WHERE user_id=".$this->user->user_id;
+
+				$users = DB::instance(DB_NAME)->select_rows($q);
+
+				
+
+			
+			$_POST['password'] =sha1(PASSWORD_SALT.$_POST['password']);
+
+			$_POST['token'] = sha1(TOKEN_SALT.$_POST['email'].Utils::generate_random_string());
+ 			
+ 			
+
+			 $user_id = DB::instance(DB_NAME)->insert("users", $_POST);*/
+
+			Router::redirect('/users/profile');
+
+		}
+	
+
 		public function login($error=NULL) {
 
 
@@ -130,52 +175,37 @@
 
 
 
-		public function profile() {
+		public function profile($user=NULL) {
 
 			if(!$this->user) {
 
 				Router::redirect('/users/membersonly');
 			}
+			 	 echo '<pre>';
+				print_r($this->user);
+				echo '</pre>';
 
+			/* $q = "SELECT
+				users.first_name,
+				users.last_name,
+				users.email
+				FROM users
+				WHERE user_id=".$this->user->user_id;
+
+			$users = DB::instance(DB_NAME)->select_rows($q);
+			
+			*/
+
+           
 			#setup the view
 			$this->template->content = View::instance('v_users_profile');
 			#give the page a title
 			$this->template->title = "Profile of ".$this->user->first_name;
 			
 
-			#setup the client fild head
-			/* $client_files_head = Array(
-				'/css/profile.css',
-				'/css/master.css'
-				);
-			$this->template->client_files_head = Utils::load_client_files($client_files_head);
-			#setup the client fild body
-			$client_files_body = Array(
-				'/css/profile.js',
-				'/css/master.js'
-				);
-			$this->template->client_files_body = Utils::load_client_files($client_files_body);
-
-
-			#pass the data to the view
-			$this->template->content->user_name=$user_name;
-			*/
-			#display the view
 			echo $this->template;
 
-			
-			//$view = View::instance('v_users_profile');	
-			//$view->user = $user;
-			//echo $view;
 
-			
-			//if($user_name == null) {
-			//	echo "No user specified";
-			//}
-			//else {
-			//	echo "This is the profile ".$user_name;
-			//}
-			
 		}
 
 		public function membersonly() {
@@ -194,3 +224,37 @@
 
 
 } #eoc
+
+
+#shit I probably don't need but who knows.
+#from profile()
+			#setup the client fild head
+			/* $client_files_head = Array(
+				'/css/profile.css',
+				'/css/master.css'
+				);
+			$this->template->client_files_head = Utils::load_client_files($client_files_head);
+			#setup the client fild body
+			$client_files_body = Array(
+				'/css/profile.js',
+				'/css/master.js'
+				);
+			$this->template->client_files_body = Utils::load_client_files($client_files_body);
+
+
+			#pass the data to the view
+			$this->template->content->user_name=$user_name;
+
+
+			
+			$view = View::instance('v_users_profile');	
+			$view->user = $user;
+			echo $view;
+
+			
+			if($user_name == null) {
+				echo "No user specified";
+			}
+			else {
+				echo "This is the profile ".$user_name;
+			}			*/
--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -1,6 +1,56 @@
 <?php if(isset($user)): ?>
+
+
     <h2>Welcome back <?=$user->first_name?>!</h2>
+	
+
+	<h3>Update your information here</h3>
+	<form class="form-horizontal" method='Post' action='/users/p_profile_update'>
+	<div class="control-group">
+		<label class ="control-label" for="inputFirstName">First Name &nbsp;</label>
+		<div class="controls">
+			<input type='text' id="inputFirstName" name='first_name' placeholder='<?php echo $user->first_name; ?>'>
+		</div>
+	</div>
+	<div class="control-group">
+		<label class ="control-label" for="inputLastName">Last Name &nbsp;</label>
+		<div class="controls">
+			<input type='text'id="inputLastName" name='last_name' placeholder='<?php echo $user->last_name; ?>'>
+		</div>
+	</div>
+	<div class="control-group">
+		<label class ="control-label" for="inputEmail">Email  &nbsp;</label>
+		<div class="controls">
+			<input type='text'id="inputEmail" name='email' placeholder=' <?php echo $user->last_name; ?>'>
+		</div>
+	</div>
+	<div class="control-group">
+		<label class ="control-label" for="inputPassword">Password  &nbsp;</label>
+		<div class="controls">
+			<input type='password'id="inputPassword" name='password' placeholder="We won't show that here">
+		</div>
+	</div>
+	
+	<div class="control-group">
+		<div class="controls">
+			<button type="submit" class="btn btn-primary">Update Info</button>
+		</div>
+		<br>
+	</div>
+</form>
+
+
+
+
+
+
 <?php else: ?>
     <h1>No user specified</h1>
 <?php endif; ?>
+
+
+
+
+
+
 
